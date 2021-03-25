12185a79a076481faa982c0063b0460d63625d36
Tom Eykholt <459821@student.fontys.nl>
2021-03-25 11:05:52 +0100

    bootstrap added & link to user tabel
    Bootstrap added and link to user tabel added in controller

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class TicketController extends Controller
 {
     function getAllTickets(){
-        $data = TicketModel::all();
+        $data = TicketModel::join("person","person.id","=","support_ticket.person_id")->get();
 
         return view('dashboard')->with('results' , $data);
     }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,21 +1,29 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <title>Dashboard</title>
+        <link href="/css/app.css" rel="stylesheet">
     </head>
     <body>
-        <table>
+    <div class="container">
+        <h1 class="mt-2">Dashboard</h1>
+        <table class="table">
             <tr>
                 <th>Id</th>
+                <th>username</th>
                 <th>subject</th>
                 <th>status id</th>
             </tr>
             @foreach($results as $result)
                 <tr>
                     <td>{{$result['id']}}</td>
+                    <td>{{$result['username']}}</td>
                     <td>{{$result['subject']}}</td>
                     <td>{{$result['status_id']}}</td>
                 </tr>
             @endforeach
         </table>
+    </div>
+
     </body>
 </html>
