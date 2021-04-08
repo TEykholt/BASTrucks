aa029f82338e481c7154c36d476a3f2f36ffcc56
Tom Eykholt <459821@student.fontys.nl>
2021-04-08 13:50:17 +0200

    Styling for ticketinput
    Styling for ticketinput

--- resources/views/ticketInput.blade.php
+++ resources/views/ticketInput.blade.php
@@ -1,26 +1,36 @@
 @include("incl/header")
-        <form method="POST" action="/ticketInput/addTicket">        
+<h1>Insert Ticket</h1>
+        <form method="POST" action="/ticketInput/addTicket">
             @csrf
-
-            <label for="">subject</label><input type="text" name="subject">
-            <label for="">message</label><input type="text" name="message">
-            <label for="">person_id</label><input type="text" name="person_id">
+            <div class="form-group">
+                <label for="subject">Subject</label>
+                <input id="subject" type="text" name="subject" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="message">Message</label>
+                <input id="message" type="text" name="message" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="person_id">Person_id</label>
+                <input id="person_id" type="text" name="person_id" class="form-control">
+            </div>
 
             <?php $results = array(
                 array(
                     "id" => 1,
-                    "name" => "ICT",             
+                    "name" => "ICT",
                 )
             )?>
-            <label for="">Department</label>
-            <select name="department_id" id="Department">
-                @foreach($results as $result)
-                    <option value="{{$result['id']}}">{{$result['name']}}</option>
-                @endforeach
-            </select>
-            <input type="submit">
+            <div class="form-group">
+                <label for="department_id">Department</label>
+                <select id="department_id" class="form-control" name="department_id" id="Department">
+                    @foreach($results as $result)
+                        <option value="{{$result['id']}}">{{$result['name']}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <input type="submit" class="btn btn-primary">
         </form>
-    </div>
-
+        </div>
     </body>
 </html>
