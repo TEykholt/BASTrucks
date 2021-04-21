9b803e879bb891acf0add66cafe3699a40328e44
RIk <Rikdenbreejen@hotmail.nl>
2021-04-21 11:29:34 +0200

    [Addition] Added getAllTicketsFromUserDepartmentWithStatus

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -66,6 +66,17 @@ class TicketController extends Controller
         return view('dashboard')->with('results' , $data);
     }
 
+    function getAllTicketsFromUserDepartmentWithStatus($Status) {
+        $data = TicketModel::join("person","person.id","=","support_ticket.person_id")
+            ->join("department","department.id","=","support_ticket.department_id")
+            ->select('support_ticket.id', 'status', 'subject', 'type', 'message', 'person.name as person_name', 'department.name as department_name')
+            ->where('department.name', 'ICT')
+            ->where('support_ticket.status', $Status)
+            ->get();
+        
+        return view('dashboard')->with('results' , $data);
+    }
+
     function GetSingle(Request $repuest) {
         $data = TicketModel::join("person","person.id","=","support_ticket.person_id")
             ->join("department","department.id","=","support_ticket.department_id")
