4e02bf2ff810bd4697f7997ac7f939005ccda93c
Rens Vlooswijk <rensvlooswijK@gmail.com>
2021-04-29 14:10:30 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ resources/views/Feedback.blade.php
@@ -0,0 +1,31 @@
+@include("incl/header")
+        <h1 class="mt-2">Feedback</h1>
+        <form method="POST" id="input_form" action="/Feedback/new" enctype="multipart/form-data">
+            @csrf
+        <div class="Filter-Item ">
+            <label for="pros" class="Filter-Label">Pro's</label>
+            <textarea name="pros" type="text" class="Filter-Value VerticalResize DisplayBlock"cols="40" rows="5" value=""></textarea>
+        </div>
+        <div class="Filter-Item ">
+            <label for="cons" class="Filter-Label">Con's</label>
+            <textarea name="cons" type="text" class="Filter-Value VerticalResize DisplayBlock"cols="40" rows="5" value=""></textarea>
+        </div>
+
+        <select class = "Score">
+
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+        </select>
+        <Button type='submit' name='SubmitFeedback'> Submit </Button>
+        </form>
+    </div>
+    </body>
+</html>
