1c6b1d99348879d619e97fb592e42c6d731cbda5
Jimmy Schuurmans <jimmysch123@gmail.com>
2021-04-29 15:36:04 +0200

    [Change] TicketPersonController update

--- app/Http/Controllers/TicketPersonController.php
+++ app/Http/Controllers/TicketPersonController.php
@@ -7,13 +7,25 @@ use Illuminate\Http\Request;
 
 class TicketPersonController extends Controller
 {
-    function TicketPersonAdd()
+    function GetAssignedTicketPersons($ticketid)
     {
-        $data=TicketPersonModel::select('id', 'ticket_id', 'person_id', 'status', 'created_at', 'updated_at');
+        $data=TicketPersonModel::select('id', 'ticket_id', 'person_id', 'status', 'created_at', 'updated_at')->where("ticket_id", $ticketid)->where('status', 'assigned');
     }
 
-    function TicketPersonRemove()
+    function TicketPersonCreate(Request $request)
     {
-        //a
+        $request->except('_token');
+        //to-do prevent unauthorized users from assigning people
+        $ticketperson = new TicketPersonModel;
+        $ticketperson->person_id = $request->person_id;
+        $ticketperson->ticket_id = $request->ticket_id;
+        $ticketperson->status = "assigned";
+        $ticketperson->save();
+    }
+
+    function TicketPersonRemove(Request $request)
+    {
+        TicketPersonModel::where('person_id', $request->person_id)->where('ticket_id', $request->ticket_id)
+        ->update(['status' => "unassigned"]);
     }
 }
