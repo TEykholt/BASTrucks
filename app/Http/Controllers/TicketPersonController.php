0cd71c8cc7a96042e4b0cbeb402f23ee59270ca7
Jimmy Schuurmans <jimmysch123@gmail.com>
2021-04-29 15:03:46 +0200

    [Add]TicketPersonModel & TicketPersonController small change

--- app/Http/Controllers/TicketPersonController.php
+++ app/Http/Controllers/TicketPersonController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class TicketPersonController extends Controller
 {
-    
+    function TicketPersonAdd()
+    {
+        $data=TicketPersonModel::select('id', 'ticket_id', 'person_id', 'status', 'created_at', 'updated_at');
+    }
+
+    function TicketPersonRemove()
+    {
+        //a
+    }
 }
---
+++ app/TicketPersonModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TicketPersonModel extends Model
+{
+    protected $table = 'ticket_person';
+    protected $primaryKey = 'id';
+}
