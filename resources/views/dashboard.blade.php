5f8afa760e0877adc9368fde4966364883c10f79
RikThePixel <Rikdenbreejen@hotmail.nl>
2021-05-12 14:00:16 +0200

    [Addition] Search on feature

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,10 +5,17 @@
     <div class="Filter-List ml-auto">
         <div class="Filter-Item">
             <!--Filter-->
-            <label for="Type" class="Filter-Label">
+            <label class="Filter-Label">
                 Search:
             </label>
             <input name="subject" class="Filter-Value" oninput="filterChangedText(this, this.value)"></input>
+            <label class="Filter-Label">
+                On:
+            </label>
+            <select class="Filter-Value" onchange="changeFilterName(this, this.value, this.parentNode.getElementsByTagName('INPUT')[0], filterChangedText)">
+                <option selected value="subject">Subject</option>
+                <option value="person_name">Ticket holder</option>
+            </select>
         </div>
         <div class="Filter-Item">
             <!--Filter-->
@@ -91,6 +98,15 @@
     // Filter
     //
     var filterOptions = {};
+    function changeFilterName(Element, newValue, ToChangeElement, filterUpdateFunction){
+        if (ToChangeElement) {
+            removeFilter(ToChangeElement.getAttribute("name"));
+            ToChangeElement.setAttribute("name", newValue);
+            if (filterUpdateFunction) {
+                filterUpdateFunction(ToChangeElement, ToChangeElement.value);
+            }
+        }
+    }
 
     function filterChanged(Element, Value, ValidationFunction) {
         var Name = Element.getAttribute("name");
@@ -98,15 +114,20 @@
             return null;
         }
 
-        addFilter(Name, Value, ValidationFunction);
+        ColumnNames = Name.split("/")
+        for (let index = 0; index < ColumnNames.length; index++) {
+            const Col = ColumnNames[index];
+            addFilter(Col, Value, ValidationFunction);
+        }
         filterTickets();
     }
 
     function filterChangedText(Element, Value) {
         filterChanged(Element, Value, (CellName, CellValue, FilterValue) => {
-
+            if (FilterValue == null) {
+                return true;
+            }
             if (FilterValue.trim() == "") {
-                console.log("aW");
                 return true;
             }
 
@@ -121,6 +142,11 @@
         };
         filterOptions[Name] = newFilter;
         return newFilter;
+    }
+    function removeFilter(Name) {
+        if (filterOptions[Name]) {
+            delete filterOptions[Name];
+        }
     }
 
     function adheresToFilter(Row) {
@@ -165,7 +191,7 @@
         if (!Table) {
             return null;
         }
-
+        
         var TableBody = Table.firstElementChild;
         var TableRows = TableBody.getElementsByClassName("trow");
         for (let index = 0; index < TableRows.length; index++) {
