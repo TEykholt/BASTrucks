f2282c302d369a491bf98d4ee0e2f27fe62ff0fe
Rens Vlooswijk <rensvlooswijk@gmail.com>
2021-05-27 15:05:25 +0200

    Auto stash before merge of "master" and "origin/master"

--- resources/views/Feedback.blade.php
+++ resources/views/Feedback.blade.php
@@ -2,15 +2,11 @@
         <h1 class="mt-2">Feedback</h1>
         <form method="POST" id="input_form" action="/Feedback/new" enctype="multipart/form-data">
             @csrf
-        <div class="Filter-Item ">
-            <label for="pros" class="Filter-Label">Pro's</label>
-            <textarea name="pros" type="text" class="Filter-Value VerticalResize DisplayBlock"cols="40" rows="5" value=""></textarea>
+        <div id="FeedbackBox" class="Filter-Item ">
+            <label for="FeedbackBox" class="Filter-Label">Feedback</label>
+            <textarea name="FeedbackBox" type="text" class="Filter-Value VerticalResize DisplayBlock"cols="40" rows="5" value=""></textarea>
         </div>
-        <div class="Filter-Item">
-            <label for="cons" class="Filter-Label">Con's</label>
-            <textarea name="cons" type="text" class="Filter-Value VerticalResize DisplayBlock"cols="40" rows="5" value=""></textarea>
-        </div>
-
+        <h3>What score would you give your ticket resolution</h3>
         <select class = "Score" name="Score">
             <option value="1">1</option>
             <option value="2">2</option>
@@ -23,9 +19,29 @@
             <option value="9">9</option>
             <option value="10">10</option>
         </select>
-        <Input value="{{$ticket_id}}" Name="ticket_id" hidden></Input>
-        <Button type='submit' name='SubmitFeedback'> Submit </Button>
+        <h3>and was there anything not satisfactory</h3>
+        <div id="myRadioGroup">
+            <input type="radio" name="Feedback" id="Yes"value="Yes">Yes
+            <input type="radio" name="Feedback" id="No" checked="checked" value="No">No    
+        </div>
+
+        <input value="{{$ticket_id}}" Name="ticket_id"></input>
+        <button type='submit' name='SubmitFeedback'> Submit </button>
+    </div>
         </form>
     </div>
+    <script>
+        $(document).ready(function() {
+            $("#FeedbackBox").hide();
+        $('input:radio[name=Feedback]').change(function() {
+            if (this.value == 'Yes') {
+                $("#FeedbackBox").show();
+                } 
+            else if(this.value == 'No'){
+                $("#FeedbackBox").hide();
+            }
+            });
+        });
+        </script>
     </body>
 </html>
