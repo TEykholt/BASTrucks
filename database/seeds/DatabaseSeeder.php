cedce09328b062ab5a4995b77bac7982b06c417d
RIk <Rikdenbreejen@hotmail.nl>
2021-05-27 14:41:27 +0200

    [Setup] Spatie Permissions

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // create permissions
+        Permission::create(['name' => 'ticket input']);
+        Permission::create(['name' => 'view own tickets']);
+        Permission::create(['name' => 'view own department tickets']);
+        Permission::create(['name' => 'view all tickets']);
+
+        Permission::create(['name' => 'edit ticket status']);
+        Permission::create(['name' => 'edit ticket types']);
+        Permission::create(['name' => 'edit ticket']);
+        
+        Permission::create(['name' => 'assign employee']);
+        Permission::create(['name' => 'unassign employee']);
+        Permission::create(['name' => 'edit employee']);
+
         // $this->call(UserSeeder::class);
     }
 }
