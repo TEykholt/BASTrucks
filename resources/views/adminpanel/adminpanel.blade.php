d938f8d4aa288abbc63ff36aa3ca26dd0a2b20ed
Tom Eykholt <459821@student.fontys.nl>
2021-05-31 09:29:38 +0200

    styling

--- resources/views/adminpanel/adminpanel.blade.php
+++ resources/views/adminpanel/adminpanel.blade.php
@@ -1,38 +1,46 @@
 @include("incl/header")
 
 
-<div class="container-fluid text-center">
     <h1>Admin Panel</h1>
-    <div class="row">
-        <div class="col-4">
-            <h2>ticket types</h2>
-            <p>Discription</p>
-            <p><a href="/ticketType" class="btn btn-dark btn-large">Mange</a><p>
+    <div class="row a-panel">
+        <div class="card  m-2" style="width: 20rem;">
+            <div class="card-body">
+                <h5 class="card-title">Ticket types</h5>
+                <p class="card-text">Edit the type of the ticket.</p>
+                <a href="/ticketType" class="btn btn-primary">Mange</a>
+            </div>
         </div>
-        <div class="col-4">
-            <h2>Status</h2>
-            <p>Discription</p>
-            <p><a href="/ticketType" class="btn btn-dark btn-large">Mange</a><p>
+        <div class="card m-2" style="width: 20rem;">
+            <div class="card-body">
+                <h5 class="card-title">Status</h5>
+                <p class="card-text">Edit the type of the avaible statuses.</p>
+                <a href="/ticketType" class="btn btn-primary">Mange</a>
+            </div>
         </div>
-        <div class="col-4">
-            <h2>Kpi</h2>
-            <p>Discription</p>
-            <p><a href="/ticketType" class="btn btn-dark btn-large">Mange</a><p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-4">
-            <h2>Departments</h2>
-            <p>Discription</p>
-            <p><a href="/ticketType" class="btn btn-dark btn-large">Mange</a><p>
-        </div>
-        <div class="col-4">
-            <h2>Users</h2>
-            <p>Discription</p>
-            <p><a href="/ticketType" class="btn btn-dark btn-large">Mange</a><p>
+        <div class="card m-2" style="width: 20rem;">
+            <div class="card-body">
+                <h5 class="card-title">Kpi</h5>
+                <p class="card-text">Edit the type of the avaible kpi.</p>
+                <a href="/ticketType" class="btn btn-primary">Mange</a>
+            </div>
         </div>
     </div>
-</div>
+    <div class="row a-panel">
+        <div class="card m-2" style="width: 20rem;">
+            <div class="card-body">
+                <h5 class="card-title">Departments</h5>
+                <p class="card-text">Edit the type of the departments kpi.</p>
+                <a href="/ticketType" class="btn btn-primary">Mange</a>
+            </div>
+        </div>
+        <div class="card col-3 m-2" style="width: 20rem;">
+            <div class="card-body">
+                <h5 class="card-title">Users</h5>
+                <p class="card-text">Edit the type of the departments kpi.</p>
+                <a href="/ticketType" class="btn btn-primary">Mange</a>
+            </div>
+        </div>
+    </div>
 
 </div>
 </body>
