fa4a1fdc47566fea5ec6d9c9fb594d1f6ee99918
Tjerk <t.zeilstra@student.fontys.nl>
2021-05-31 13:28:18 +0200

    adminpanel redirect

--- resources/views/adminpanel/adminpanel.blade.php
+++ resources/views/adminpanel/adminpanel.blade.php
@@ -7,21 +7,21 @@
             <div class="card-body">
                 <h5 class="card-title">Ticket types</h5>
                 <p class="card-text">Edit the type of the ticket.</p>
-                <a href="/ticketType" class="btn btn-primary">Mange</a>
+                <a href="/admin/ticketType" class="btn btn-primary">Mange</a>
             </div>
         </div>
         <div class="card m-2" style="width: 20rem;">
             <div class="card-body">
                 <h5 class="card-title">Status</h5>
                 <p class="card-text">Edit the type of the avaible statuses.</p>
-                <a href="/ticketStatus" class="btn btn-primary">Mange</a>
+                <a href="/admin/ticketStatus" class="btn btn-primary">Mange</a>
             </div>
         </div>
         <div class="card m-2" style="width: 20rem;">
             <div class="card-body">
                 <h5 class="card-title">Kpi</h5>
                 <p class="card-text">Edit the type of the avaible kpi.</p>
-                <a href="/ticketType" class="btn btn-primary">Mange</a>
+                <a href="/admin/ticketType" class="btn btn-primary">Mange</a>
             </div>
         </div>
     </div>
@@ -30,14 +30,14 @@
             <div class="card-body">
                 <h5 class="card-title">Departments</h5>
                 <p class="card-text">Edit the type of the departments kpi.</p>
-                <a href="/ticketType" class="btn btn-primary">Mange</a>
+                <a href="/admin/ticketType" class="btn btn-primary">Mange</a>
             </div>
         </div>
         <div class="card m-2" style="width: 20rem;">
             <div class="card-body">
                 <h5 class="card-title">Users</h5>
                 <p class="card-text">Edit the type of the departments kpi.</p>
-                <a href="/ticketType" class="btn btn-primary">Mange</a>
+                <a href="/admin/ticketType" class="btn btn-primary">Mange</a>
             </div>
         </div>
     </div>
