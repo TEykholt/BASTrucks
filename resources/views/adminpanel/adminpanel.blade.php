4895ed1d9cfc88d01e90a0b2edd9d36d4d4eea8f
RIk <Rikdenbreejen@hotmail.nl>
2021-06-16 01:16:15 +0200

    [Fix] GRAMMAR MISTAKES!!!!

--- resources/views/adminpanel/adminpanel.blade.php
+++ resources/views/adminpanel/adminpanel.blade.php
@@ -7,21 +7,21 @@
             <div class="card-body">
                 <h5 class="card-title">Ticket types</h5>
                 <p class="card-text">Edit the type of the ticket.</p>
-                <a href="/admin/ticketTypes" class="btn btn-primary">Mange</a>
+                <a href="/admin/ticketTypes" class="btn btn-primary">Manage</a>
             </div>
         </div>
         <div class="card m-2" style="width: 20rem;">
             <div class="card-body">
-                <h5 class="card-title">Status</h5>
+                <h5 class="card-title">Ticket status</h5>
                 <p class="card-text">Edit the type of the avaible statuses.</p>
-                <a href="/admin/ticketStatus" class="btn btn-primary">Mange</a>
+                <a href="/admin/ticketStatus" class="btn btn-primary">Manage</a>
             </div>
         </div>
         <div class="card m-2" style="width: 20rem;">
             <div class="card-body">
                 <h5 class="card-title">Departments</h5>
                 <p class="card-text">Edit the type of the departments kpi.</p>
-                <a href="/admin/departments" class="btn btn-primary">Mange</a>
+                <a href="/admin/departments" class="btn btn-primary">Manage</a>
             </div>
         </div>
     </div>
@@ -30,7 +30,7 @@
             <div class="card-body">
                 <h5 class="card-title">Users</h5>
                 <p class="card-text">Edit the type of the departments kpi.</p>
-                <a href="/admin/users" class="btn btn-primary">Mange</a>
+                <a href="/admin/users" class="btn btn-primary">Manage</a>
             </div>
         </div>
     </div>
